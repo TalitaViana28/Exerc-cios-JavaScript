57d1029ef48259a34149c8a6dc5201ad600f7d4c
Talita Viana <talitasaraiva@outlook.com.br>
2023-06-26 11:10:57 -0300

    thirteenth commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-var texto =
-  "Fairfax Harrison foi um advogado, empresário e escritor americano. Filho do secretário confederado Jefferson Davis, Harrison estudou Direito na Universidade Yale e na Universidade Columbia antes de se tornar advogado da Southern Railway Company em 1896. Em 1906, ele era vice-presidente do setor ferroviário da Southern e, no ano seguinte, ajudou a garantir financiamento para manter a empresa solvente. Em 1913, foi eleito presidente da Southern, onde instituiu uma série de reformas no funcionamento da empresa.";
-console.log(texto);
+(function (win, doc) {
+  "use strict";
+
+  var $inputUsername = doc.querySelector("#username");
+  var $inputPassword = doc.querySelector("#password");
+  var $button = doc.querySelector("#button");
+
+  $button.addEventListener(
+    "su",
+    function (event) {
+      event.preventDefault();
+      console.log("Click no botão");
+    },
+    false
+  );
+})(window, document);
