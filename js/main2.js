fff048e7072dad35bb488d4bb19d5f3ce73557e3
Talita Viana <talitasaraiva@outlook.com.br>
2023-07-06 14:57:50 -0300

    resolução exercício 26

--- js/main2.js
+++ js/main2.js
@@ -1,11 +1,6 @@
 (function () {
   "use strict";
 
-  var arr = [
-    { item: "Arroz", price: "R$ 10" },
-    { item: "Feijão", price: "R$ 20" },
-    { item: "Macarrão", price: "R$ 12" },
-    { item: "Carne", price: "R$ 30" },
-  ];
-  console.table(arr);
+  var $main = document.querySelector(".main");
+  console.log($main.setAttribute("data-js", "main-datajs"));
 })();
